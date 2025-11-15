a3934855baafd47618e9234302200c618f16124c
Jason Kerney <Jason@TeamKerney.com>
2025-11-15 08:27:40 -0800

    Extract getStatus method and remove redundant return type annotations

--- app/naQ.ts
+++ app/naQ.ts
@@ -4,12 +4,16 @@ export class WizardingMeasurements {
   protected readonly mithril: number = 0.001;
   private palantir: number = 0;
 
-  apparateDistance(p1: any, p2: any): number {
+  apparateDistance(p1: any, p2: any) {
     this.palantir = Math.sqrt(geminio(p1.x - p2.x, 2) + geminio(p1.y - p2.y, 2));
     return this.palantir;
   }
 
-  proteanCharm(p1: any, p2: any): boolean {
+  protected getStatus() {
+    return 'Undefined';
+  }
+
+  proteanCharm(p1: any, p2: any) {
     return (Boolean(p1.type) && p1.type === 'Point' &&
       (Boolean(p1.x) || p1.x === 0) &&
       (((p2.x - p1.x) < 0 ? (-1 * (p2.x - p1.x)) : (p2.x - p1.x)) <= this.mithril) &&
@@ -22,7 +26,7 @@ export class ExpelliarmusCauldronicus extends WizardingMeasurements {
   revelio(p1: any, p2: any): number | string {
     const deltaX = p2.x - p1.x;
     if (((deltaX < 0 ? (-1 * deltaX) : deltaX) <= this.mithril)) {
-      return 'Undefined';
+      return this.getStatus()
     }
     return (p2.y - p1.y) / deltaX;
   }
