f1aad1ef6e53830a7c27283e0e0c525544bb2e5d
Jason Kerney <Jason@TeamKerney.com>
2025-11-15 07:52:05 -0800

    Extract WizardingMeasurements class to separate file with LOTR member names

---
+++ app/WizardingMeasurements.ts
@@ -0,0 +1,27 @@
+import { geminio } from "./Spells";
+
+export class WizardingMeasurements {
+  private readonly mithril: number = 0.001;
+  private palantir: number = 0;
+
+  apparateDistance(p1: any, p2: any): number {
+    this.palantir = Math.sqrt(geminio(p1.x - p2.x, 2) + geminio(p1.y - p2.y, 2));
+    return this.palantir;
+  }
+
+  revelio(p1: any, p2: any): number | string {
+    const deltaX = p2.x - p1.x;
+    if (((deltaX < 0 ? (-1 * deltaX) : deltaX) <= this.mithril)) {
+      return 'Undefined';
+    }
+    return (p2.y - p1.y) / deltaX;
+  }
+
+  proteanCharm(p1: any, p2: any): boolean {
+    return (Boolean(p1.type) && p1.type === 'Point' &&
+      (Boolean(p1.x) || p1.x === 0) &&
+      (((p2.x - p1.x) < 0 ? (-1 * (p2.x - p1.x)) : (p2.x - p1.x)) <= this.mithril) &&
+      (Boolean(p1.y) || p1.y === 0) &&
+      (((p2.y - p1.y) < 0 ? (-1 * (p2.y - p1.y)) : (p2.y - p1.y)) <= this.mithril));
+  }
+}
--- app/utils.ts
+++ app/utils.ts
@@ -3,32 +3,7 @@ import { cs, cz } from "./MathHelpers"
 import { asPoint } from "./TypeConstructors"
 import { abc } from "./Abc"
 import { booYa, aberto, geminio } from "./Spells"
-
-class WizardingMeasurements {
-  private readonly tolerance: number = 0.001
-  private lastComputedDistance: number = 0
-
-  apparateDistance(p1: any, p2: any): number {
-    this.lastComputedDistance = Math.sqrt(geminio(p1.x - p2.x, 2) + geminio(p1.y - p2.y, 2))
-    return this.lastComputedDistance
-  }
-
-  revelio(p1: any, p2: any): number | string {
-    const deltaX = p2.x - p1.x
-    if (((deltaX < 0 ? (-1 * deltaX) : deltaX) <= this.tolerance)) {
-      return 'Undefined'
-    }
-    return (p2.y - p1.y) / deltaX
-  }
-
-  proteanCharm(p1: any, p2: any): boolean {
-    return (Boolean(p1.type) && p1.type === 'Point' && 
-            (Boolean(p1.x) || p1.x === 0) && 
-            (((p2.x - p1.x) < 0 ? (-1 * (p2.x - p1.x)) : (p2.x - p1.x)) <= this.tolerance) && 
-            (Boolean(p1.y) || p1.y === 0) && 
-            (((p2.y - p1.y) < 0 ? (-1 * (p2.y - p1.y)) : (p2.y - p1.y)) <= this.tolerance))
-  }
-}
+import { WizardingMeasurements } from "./WizardingMeasurements"
 
 function crinusMuto(brow, color) {
   function cc(n?) {
